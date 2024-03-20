2c73350073ae64e9a3b4fcf4be81380186b894b8
001183643 <zeinab.ziabari@student.tafesa.edu.au>
2024-03-20 20:01:43 +1030

    More discount

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -14,9 +14,9 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 40.5m;
-        const decimal mdecGeneral = 30.75m;
-        const decimal mdecBox = 62.0m;
+        const decimal mdecBalcony = 30.5m;
+        const decimal mdecGeneral = 20.75m;
+        const decimal mdecBox = 50.0m;
         const decimal mdecDiscount = 5.0m; 
 
         private int Section
